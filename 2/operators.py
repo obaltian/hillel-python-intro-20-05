56318e347946261b6030f4c3aea4d423ff0c55b0
Oleksandr Baltian <oleksandr.baltian@maklai.com>
2022-05-23 18:42:54 +0200

    add materials from 2nd lecture

---
+++ 2/operators.py
@@ -0,0 +1,54 @@
+# Arithmetic
+"""
+    +       "hello" + "hello" = "hellohello"
+    -
+    *       "hello" * 3 = "hellohellohello"
+    /        10 / 3  = 3.333333333
+    //       10 // 3 = 3
+    %        10 % 3 = 1
+    **
+"""
+
+a = 125
+print(a % 10)
+print(a % 100)
+
+print(a % 2 == 0)
+print(100 % 2 == 0)
+
+print(2 ** 2 ** 3)
+tmp = 2 ** 2
+print(tmp ** 3)
+
+a = 5 / 100 + 3
+
+
+# Comparison
+"""
+    ==
+    !=
+    <
+    <=
+    >
+    >=
+"""
+
+# Identity
+"""
+    is 
+    is not
+"""
+
+# Logical
+"""
+    and
+    or
+    not
+"""
+
+# Membership
+"""
+    in
+    not in
+"""
+
